f7c5abe52bd1011b6e728baef87f4ec46a7d2d58
鱼骨头 <1220253798@qq.com>
2022-04-13 23:08:44 +0800

    chore:

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 130,
+  endOfLine: 'auto',
+  bracketSpacing: true, // 对象中打印空格
+  jsxSingleQuote: true,
+  insertPragma: false, // 插入@format标识
+};
