e564b3cc235adb86152ed7c610ba2dc10bea7b26
鱼骨头 <1220253798@qq.com>
2022-05-28 14:45:58 +0800

    feat: plum

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,20 @@ const { push } = useRouter()
 function toMineSweeper() {
   push('/minesweeper')
 }
+function toPlum() {
+  push('/canvas-plum')
+}
 </script>
 
 <template>
-  <button bg-green-400 w-20 h-10 rounded-5 @click="toMineSweeper">
-    跳转
-  </button>
+  <div flex gap-1 justify-center>
+    <button bg-green-400 w-20 h-10 rounded-5 @click="toMineSweeper">
+      扫雷
+    </button>
+    <button bg-green-400 w-20 h-10 rounded-5 @click="toPlum">
+      梅花动画
+    </button>
+  </div>
 </template>
 
 <style lang='less' scoped>
