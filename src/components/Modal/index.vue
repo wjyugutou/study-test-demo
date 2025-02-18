6836286839993cbb6a9748c6b73fe4fb9ae19c32
WJ <1220253798@qq.com>
2025-02-18 14:11:46 +0800

    feat: useModal

--- src/components/Modal/index.vue
+++ src/components/Modal/index.vue
@@ -3,9 +3,19 @@ import type { CSSProperties, Ref } from 'vue'
 
 defineOptions({ name: 'Modal' })
 
-const props = withDefaults(defineProps<{
+const props = withDefaults(defineProps<Props>(), {
+  width: 500,
+  showMask: true,
+  drag: true,
+  clickMaskClose: true,
+})
+const emits = defineEmits<{
+  (e: 'update:modelValue', visible: boolean): void
+}>()
+
+export interface Props {
   modelValue: boolean
-  appendTo?: string | Element
+  appendTo?: string | HTMLElement
   title: string
   width?: number
   showMask?: boolean
@@ -16,16 +26,7 @@ const props = withDefaults(defineProps<{
   class?: string
   style?: CSSProperties
   content?: string | number
-}>(), {
-  width: 500,
-  showMask: true,
-  drag: true,
-  clickMaskClose: true,
-})
-
-const emits = defineEmits<{
-  (e: 'update:modelValue', visible: boolean): void
-}>()
+}
 
 const dragEle = ref() as Ref<HTMLElement>
 
@@ -52,33 +53,26 @@ function clickMaskHandle() {
 
 <template>
   <Teleport :to="appendTo" :disabled="!appendTo">
-    <Transition name="modal_fade">
+    <Transition name="modal-fade">
       <Overlayer v-if="modelValue" :mask="showMask" :mask-class="maskClass" @click="clickMaskHandle">
-        <div :x="x" :y="y" :style="modalContainerStyle" class="modal_container" :class="props.class">
-          <header ref="dragEle" class="modal_header" :style="{ cursor: drag ? 'move' : 'default' }">
+        <div :x="x" :y="y" :style="modalContainerStyle" class="modal-container" :class="props.class">
+          <header ref="dragEle" class="modal-header" :style="{ cursor: drag ? 'move' : 'default' }">
             <template v-if="$slots.header">
-              <slot name="header" />
+              <slot name="header">
+                <span class="modal-title-text">{{ title }}</span>
+              </slot>
             </template>
-            <template v-else>
-              <span class="modal_title_text">{{ title }}</span>
-            </template>
-            <div
-              class="modal_close" @click="closeHandle" @pointerdown.stop="(e) => {}"
-            />
+            <div class="modal-close" @click="closeHandle" @pointerdown.stop="(e) => {}" />
           </header>
-          <div class="modal_content" :style="modalContentStyle">
-            <slot v-if="$slots.default" />
-            <template v-else>
+          <div class="modal-content" :style="modalContentStyle">
+            <slot v-if="$slots.default">
               {{ content }}
-            </template>
+            </slot>
           </div>
 
-          <footer class="modal_footer" :class="footerClass">
+          <footer class="modal-footer" :class="footerClass">
             <template v-if="$slots.footer">
               <slot name="footer" />
-            </template>
-            <template v-else>
-              <div>footer</div>
             </template>
           </footer>
         </div>
@@ -88,35 +82,35 @@ function clickMaskHandle() {
 </template>
 
 <style>
-.modal_container {
+.modal-container {
   @apply: min-w-400px w-fit border-1 border-gray-400 border-rd-10px overflow-hidden z-[var(--modal-z-index)] mx-auto mt-15vh;
 }
 
-.modal_header {
-  @apply: pl-2 pr-10 text-7 border-b-1 border-gray-400 relative  bg-[var(--modal-header-bg)];
+.modal-header {
+  @apply: h-10 pl-2 pr-10 text-7 border-b-1 border-gray-400 relative  bg-[var(--modal-header-bg)];
 }
 
-.modal_title_text {
+.modal-title-text {
   user-select: none;
 }
 
-.modal_close {
+.modal-close {
   @apply: absolute top-50% right-2 translate-y--50% i-carbon-close hover:text-#fff cursor-pointer text-26px;
 }
 
-.modal_content {
+.modal-content {
   @apply: p-4 bg-[var(--modal-content-bg)]
 }
 
-.modal_footer {
+.modal-footer {
   @apply: p-4  bg-[var(--modal-footer-bg)];
 }
 
-.modal_fade-enter-active {
+.modal-fade-enter-active {
   animation: modal-enter 0.4s;
 }
 
-.modal_fade-leave-active {
+.modal-fade-leave-active {
   animation: modal-enter 0.4s reverse;
 }
 
---
+++ src/hooks/useModal.ts
@@ -0,0 +1,92 @@
+import type { Props as ModalProps } from '@/components/Modal/index.vue'
+import Modal from '@/components/Modal/index.vue'
+import { render } from 'vue'
+
+interface CreateContainerOptions {
+  id?: string
+  className?: string
+  tag?: string
+}
+
+const containerMap: Map<string, HTMLElement> = new Map()
+
+function createContainer(options: CreateContainerOptions) {
+  if (options.id && containerMap.has(options.id)) {
+    return containerMap.get(options.id)!
+  }
+
+  const container = document.createElement(options.tag || 'div')
+  const id = options.id || `modal-${Date.now()}`
+  container.id = id
+  containerMap.set(id, container)
+  options.className && (container.className = options.className)
+  return container
+}
+
+interface ModalOptions {
+  props?: Partial<ModalProps>
+  [name: string]: unknown
+}
+
+const modalRootId = 'modal-root-container'
+
+function getModalRootContainer() {
+  const modalRootContainer = document.getElementById(modalRootId) || createContainer({
+    tag: 'div',
+    id: modalRootId,
+    className: 'modal-root-container',
+  })
+  document.body.appendChild(modalRootContainer)
+
+  return modalRootContainer
+}
+
+function getAppendContainer(appendTo: ModalProps['appendTo']) {
+  let appendContainer: HTMLElement
+  if (appendTo && typeof appendTo === 'string') {
+    const el: HTMLElement | null = document.querySelector(appendTo)
+    if (!el)
+      throw new Error('appendTo element not found')
+    appendContainer = el
+  }
+  else if (appendTo && typeof appendTo === 'object') {
+    appendContainer = appendTo as HTMLElement
+  }
+  else {
+    appendContainer = getModalRootContainer()
+  }
+  return appendContainer
+}
+
+export function useModal(options: ModalOptions) {
+  const container = createContainer({
+    className: 'modal-container',
+  })
+  const rootContainer = getAppendContainer(options.props?.appendTo)
+
+  const vNode = h(Modal, {
+    'modelValue': true,
+    ...(options.props || {}),
+    'onUpdate:modelValue': (val: boolean) => {
+      if (!val)
+        close()
+    },
+  } as ModalProps)
+
+  open()
+
+  function open() {
+    render(vNode, container)
+    rootContainer.appendChild(container)
+  }
+
+  function close() {
+    container.remove()
+    containerMap.delete(container.id)
+  }
+
+  return {
+    open,
+    close,
+  }
+}
