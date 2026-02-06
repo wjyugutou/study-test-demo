8a20e1deba50842144e64178199b852522ef0eca
wwwwj <1220253798@qq.com>
2026-02-06 10:43:39 +0800

    fix(样式/错误页面): 修复错误页面样式并添加返回首页功能
    在全局样式中为按钮添加指针样式，并优化错误页面布局
    添加返回首页的按钮点击事件处理函数
    完善错误信息的显示格式

--- app/error.vue
+++ app/error.vue
@@ -4,17 +4,30 @@ import type { NuxtError } from '#app'
 defineProps({
   error: Object as () => NuxtError,
 })
+
+function handleError() {
+  clearError({
+    redirect: '/',
+  })
+}
 </script>
 
 <template>
-  <div class="pt-80 flex-center flex-col">
+  <div class="flex-center flex-col h-100vh">
     <h1 class="text-3xl font-bold">
-      Error  Page
+      Error Page
     </h1>
-    <h1>{{ error?.statusCode || '500' }}</h1>
-    <h1>{{ error?.message || 'Internal Server Error' }}</h1>
-    <NuxtLink to="/demoChallenge/autoNavBar" class="color-(green) hover:color-green-500">
+    <h1>ErrorData: {{ error?.data }}</h1>
+    <h1>ErrorCode: {{ error?.statusCode || '500' }}</h1>
+    <h1>ErrorMessage: {{ error?.message || 'Internal Server Error' }}</h1>
+    <button class="color-(green) hover:color-green-500" @click="handleError">
       返回首页
-    </NuxtLink>
+    </button>
   </div>
 </template>
+
+<style scoped>
+  h1 {
+    margin-bottom: 20px;
+  }
+</style>
