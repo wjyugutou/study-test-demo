c53a99a23be35a1b4420d6efdcb311eaebaa2282
wwwwj <1220253798@qq.com>
2026-02-06 10:25:51 +0800

    feat: 测试不推送Tag 不执行Action

--- app/error.vue
+++ app/error.vue
@@ -8,7 +8,9 @@ defineProps({
 
 <template>
   <div class="pt-80 flex-center flex-col">
-    <h1>Error Page</h1>
+    <h1 class="text-3xl font-bold">
+      Error  Page
+    </h1>
     <h1>{{ error?.statusCode || '500' }}</h1>
     <h1>{{ error?.message || 'Internal Server Error' }}</h1>
     <NuxtLink to="/demoChallenge/autoNavBar" class="color-(green) hover:color-green-500">
